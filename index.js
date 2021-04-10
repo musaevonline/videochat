e3ad9b1a329e2ceb9d0b0b94ff659a9c38aa5f8d
Amir <amir@musaev.online>
2021-04-10 11:32:08 +0300

    feat(index.html): add server env support

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get("/", (req, res) => {
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@900&display=swap" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script defer src="/main.js"></script>
+    <script defer src="/${process.env.APP_ENV === 'server' ? 'videochat/' : ''}main.js"></script>
   </head>
   <body style="margin: 0">
     <div id="app" style="overflow: hidden;"></div>
